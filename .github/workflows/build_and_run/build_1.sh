c475deac7cc31d2e4db2659ad7e04b78a3c3ab26
Michael T. Kloos <michael@michaelkloos.com>
2024-01-10 13:38:35 -0500

    Fix script and update names.

--- .github/workflows/build_and_run/build_1.sh
+++ .github/workflows/build_and_run/build_1.sh
@@ -8,6 +8,6 @@ set -x
 ARCH=riscv CROSS_COMPILE=riscv64-linux-gnu- make clean
 ARCH=riscv CROSS_COMPILE=riscv64-linux-gnu- make mrproper
 ARCH=riscv CROSS_COMPILE=riscv64-linux-gnu- make allnoconfig
-ARCH=riscv CROSS_COMPILE=riscv64-linux-gnu- make make -j$(nproc) all
+ARCH=riscv CROSS_COMPILE=riscv64-linux-gnu- make -j$(nproc) all
 
 exit 0
