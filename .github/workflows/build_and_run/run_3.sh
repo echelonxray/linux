3f81ee9d79d78ae64627584296c790eff709c171
Michael T. Kloos <michael@michaelkloos.com>
2024-01-16 16:38:41 -0500

    Update allyesconfig (run 3) script.

--- .github/workflows/build_and_run/run_3.sh
+++ .github/workflows/build_and_run/run_3.sh
@@ -5,9 +5,11 @@
 set -e
 set -x
 
-qemu-system-riscv64 -M virt -m 128M -cpu rv64 -display none -serial stdio -no-reboot \
+timeout --foreground --kill-after=60s --signal=SIGTERM 60s \
+                    qemu-system-riscv64 \
+                    -M virt -m 1G -cpu rv64 -display none -serial stdio -no-reboot \
                     -bios "/usr/lib/riscv64-linux-gnu/opensbi/generic/fw_dynamic.bin" \
-					-kernel "./arch/riscv/boot/Image" \
-					-append "panic=1"
+                    -kernel "./arch/riscv/boot/Image" \
+                    -append "panic=1"
 
 exit 0
