1f763a1f94bc84e4c9508e910ae95cfb9d7d70d7
Michael T. Kloos <michael@michaelkloos.com>
2024-01-22 19:46:53 -0500

    Create separate 32-bit and 64-bit versions of the initramfs.

--- .github/workflows/build_and_run/run_2.sh
+++ .github/workflows/build_and_run/run_2.sh
@@ -11,7 +11,7 @@ timeout --foreground --kill-after=60s --signal=SIGTERM 120s \
     qemu-system-riscv64 -M virt -m 128M -cpu rv64 -display none -serial stdio -no-reboot \
     -bios "/usr/lib/riscv64-linux-gnu/opensbi/generic/fw_dynamic.bin" \
     -kernel "./arch/riscv/boot/Image" \
-    -initrd "./.github/workflows/build_and_run/test_initramfs.cpio.gz" \
+    -initrd "./.github/workflows/build_and_run/test_initramfs_64.cpio.gz" \
     -append "console=ttyS0 panic=1 rdinit=/dumb-init /testprogram" | \
         tee /dev/stderr | grep -F "TRACEOUTPUT8107519475109" > /dev/null
 
