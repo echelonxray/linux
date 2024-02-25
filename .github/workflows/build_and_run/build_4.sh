04b15d079ba981a98b88ed2160d9099f0c8511fd
Michael T. Kloos <michael@michaelkloos.com>
2024-02-24 21:14:23 -0500

    Add temporary workaround for CI pipeline for bugs in the version of LLVM in the runner. Add missing dependencies to the install script.

--- .github/workflows/build_and_run/build_4.sh
+++ .github/workflows/build_and_run/build_4.sh
@@ -7,10 +7,12 @@ set -x
 
 if [ "$1" = "llvm" ]; then
 	# llvm
-	ARCH=riscv LLVM=1 make clean
-	ARCH=riscv LLVM=1 make mrproper
-	ARCH=riscv LLVM=1 make rv32_defconfig
-	ARCH=riscv LLVM=1 make -j$(nproc) all
+	#llvm=1
+	llvm=-15 # Temporarily using version ?-15 to work around bugs in current default LLVM version on Ubuntu (Version ?-14)
+	ARCH=riscv LLVM=$llvm make clean
+	ARCH=riscv LLVM=$llvm make mrproper
+	ARCH=riscv LLVM=$llvm make rv32_defconfig
+	ARCH=riscv LLVM=$llvm make -j$(nproc) all
 else
 	# gcc
 	ARCH=riscv CROSS_COMPILE=riscv64-linux-gnu- make clean
--- .github/workflows/build_and_run/install_dep.sh
+++ .github/workflows/build_and_run/install_dep.sh
@@ -3,9 +3,10 @@
 set -e
 set -x
 
-apt-get -y -q install build-essential
+apt-get -y -q install build-essential flex bison
 apt-get -y -q install gcc-riscv64-linux-gnu
 apt-get -y -q install llvm lld clang
+apt-get -y -q install llvm-15 lld-15 clang-15 # Temporary for working around bugs in current default LLVM version on Ubuntu (Version ?-14)
 apt-get -y -q install device-tree-compiler
 apt-get -y -q install cpio
 apt-get -y -q install qemu-system-misc
